a6e3f2f0a99d8febef7ffa9f0e23c92b2f6950a9
Assaf Goldstein <assaf.goldstein@gmail.com>
2017-02-07 11:33:08 +0200

    new logo on site

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -30,7 +30,8 @@ var cbpAnimatedHeader = (function() {
 			classie.add( header, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+					classie.add( header, 'navbar-shrink' );
+			// classie.remove( header, 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
