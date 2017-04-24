4bfc094c4d6ca5c24fcd981b3da25cce6ac6b548
Assaf Goldstein <assaf.goldstein@gmail.com>
2017-04-24 10:22:39 +0300

    connect to new CRM

--- js/agency.js
+++ js/agency.js
@@ -4,8 +4,9 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+$(function () {
     // $('a.page-scroll').bind('click', function(event) {
     //     var $anchor = $(this);
     //     $('html, body').stop().animate({
@@ -15,23 +16,30 @@ $(function() {
     // });
 });
 
+
+
+$(".openModal").click(function () {
+    $('#modal-image').attr('src', $(this).data("img"));
+    $("#modalImage").modal();
+});
+
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
 //    $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+$('div.modal').on('show.bs.modal', function () {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
 });
